ee79bb9203da6f82b97782fd2e0e30543aefdc66
simplesuccessful <ben@simplesuccessful.com>
2015-09-07 16:58:08 +0100

    creation of landing_page controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Nameofapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#index'
+   root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
